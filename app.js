a04092d49a53d5c38f10fa5e6ddb7cb88c46e4fb
Dinesh Narpache <124812926+Dineshgithub97@users.noreply.github.com>
2023-05-16 22:54:47 +0530

    update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const handleSavedNews = (savedItem) => {
 const getNews = async (category = "science") => {
   newsContainer.innerHTML = "";
   await fetch(`https://inshorts.deta.dev/news?category=${category}`)
-    .then((response) => await response.json())
+    .then((response) => response.json())
     .then((data) => {
       console.log("Data", data)
       data.data.forEach((newsItem) => {
